5b0891381ef082dbaffa7c64994a4ca77fb15352
kkhhjjoo <khjoo1203@gmail.com>
2026-02-13 22:59:22 +0900

    user.api.js post에서 get으로 수정

--- routes/user.api.js
+++ routes/user.api.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const userController = require('../controllers/user.controller');
 
 //회원가입
-router.post('/', userController.createUser);
+router.get('/', userController.createUser);
 
 module.exports = router
