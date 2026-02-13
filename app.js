64e1d96b352fe1a4063729049cb172d29b7e038c
kkhhjjoo <khjoo1203@gmail.com>
2026-02-13 21:22:24 +0900

    회원가입 백엔드

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express()
+const indexRouter = require('./routes/index');
+const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json); //req.body가 객체로 인식이 됩니다
+app.use(bodyParser.json()); //req.body가 객체로 인식이 됩니다
+
+app.use('/api', indexRouter);
 
 const mongoURI = process.env.LOCAL_DB_ADDRESS
 mongoose.connect(mongoURI).then(() => console.log("mongoose connected")).catch((err) => console.log('DB connection fail', err));
