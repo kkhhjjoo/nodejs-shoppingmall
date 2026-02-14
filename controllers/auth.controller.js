48a97aa5a7bb8879dd137fdc4c5c7e368a3f0553
kkhhjjoo <khjoo1203@gmail.com>
2026-02-14 19:37:16 +0900

    로그인 백엔드

---
+++ controllers/auth.controller.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+const authController = {}
+
+authController.loginWithEmail = async (req, res) => { 
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) { 
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) { 
+        //token
+        const token = await user.generateToken();
+        return res.status(200).json({status: 'success', user, token});
+      }
+    }
+    throw new Error('유효하지 않은 이메일 또는 패스워드입니다')
+  } catch (error) { 
+    res.status(400).json({status: 'fail', error: error.message})
+  }
+}
+
+module.exports = authController
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const userSchema = Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
@@ -7,12 +10,17 @@ const userSchema = Schema({
   level: {type: String, default: "customer"} // 2types: customer, admin
 }, { timeStamps: true });
 userSchema.methods.toJSON = function () { 
-  const obj = this._doc
-  delete obj.password
-  delete obj.__v
-  delete obj.updateAt
-  delete obj.createAt
-  return obj
+  const obj = this._doc;
+  delete obj.password;
+  delete obj.__v;
+  delete obj.updateAt;
+  delete obj.createAt;
+  return obj;
+}
+
+userSchema.methods.generateToken = async function () { 
+  const token = await jwt.sign({ _id: this.id }, JWT_SECRET_KEY, { expiresIn: "1d" });
+  return token;
 }
 
 const User = mongoose.model('User', userSchema);
