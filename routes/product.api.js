7b91bedf76cf210aac4eb7dee64b5f6b9d237650
kkhhjjoo <khjoo1203@gmail.com>
2026-02-19 18:36:19 +0900

    상품 보여주기

--- routes/product.api.js
+++ routes/product.api.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post('/', authController.authenticate, authController.checkAdminPermission, productController.createProduct);
 
+router.get('/', productController.getProducts);
+
 module.exports = router;
