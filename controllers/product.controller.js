f7df9bf912c61ae0473c3bee79d00230009b1898
kkhhjjoo <khjoo1203@gmail.com>
2026-02-20 21:12:01 +0900

    페이지네이션

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require('../models/Product');
 
+const PAGE_SIZE = 5;
 const productController = {}
 productController.createProduct = async (req, res) => { 
   try {
@@ -22,9 +23,19 @@ productController.getProducts = async (req, res) => {
     // }
     const cond = name ? { name: { $regex: name, $options: 'i' } } : {};
     let query = Product.find(cond);
+    let response = { status: 'success' };
+    if (page) { 
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      //최종 몇개 페이지
+      //데이터가 총 몇개 있는지
+      const totalItemNum = await Product.find(cond).countDocuments();
+      // 데이터 총 개수 / PAGE_SIZE
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
 
-    const productList = await query.exec()
-    res.status(200).json({status: 'success', data: productList})
+    const productList = await query.exec();
+    res.status(200).json({ ...response, data: productList });
   } catch (error) { 
     res.status(400).json({ status: 'fail', error: error.message });
   }
