092b091914259fa7f882f85148acefe49457a290
kkhhjjoo <khjoo1203@gmail.com>
2026-02-27 13:23:42 +0900

    오더 status 변경하기

--- routes/order.api.js
+++ routes/order.api.js
@@ -6,5 +6,6 @@ const orderController = require('../controllers/order.controller');
 router.post('/', authController.authenticate, orderController.createOrder);
 router.get('/', authController.authenticate, orderController.getOrder);
 router.get('/admin', authController.authenticate, authController.checkAdminPermission, orderController.getOrderList);
+router.put('/:id', authController.authenticate, authController.checkAdminPermission, orderController.updateOrder);
 
 module.exports = router;
